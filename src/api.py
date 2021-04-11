a99d705d9e328d805b5e7ab8fd2cab189540636f
Alp <alp@collectivat.cat>
2021-04-11 15:22:02 +0200

    raspi paths

--- src/api.py
+++ src/api.py
@@ -5,7 +5,7 @@ import os
 app = FastAPI()
 templates = Jinja2Templates(directory='templates/')
 
-pdsendpath = "/Applications/Pd-extended.app/Contents/Resources/bin/pdsend"
+pdsendpath = "/usr/bin/pdsend"
 
 effects = {1:"Clean", 2:"Chorus", 3:"Warble Chorus", 4:"Tremolo", 5:"Vibrato", 6:"Wah", 7:"Ring Modulator", 8:"Low Fuzz Wah", 9:"Med Fuzz Wah", 
            10:"Hi Fuzz Wah", 11:"Ring Mod Fuzz", 12:"Fuzz", 13:"Subtractive OD", 14:"Tube Screamer", 15:"Digital Delay", 16:"Tape Echo"}
